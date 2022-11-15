a9994dcde305f18e266aca255a899e0349bdcaa3
Antonio Julián Bueno Fuentes <antoniojulianbueno@gmail.com>
2022-11-16 00:23:58 +0100

    3 componentes

--- node/src/App.js
+++ node/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+  
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+  if (hours > 8 && hours < 14) {
+    timeOfDay = "Buenos dias";
+  } else if (hours >= 14 && hours < 20) {
+    timeOfDay = "Buenas tardes";
+  } else {
+    timeOfDay = "Buenas noches";
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <di>
+      <h1>React App</h1>
+      
+      <h2>Saludandos</h2>
+      {Hora()}
+      {
+        Saludo_Usuario()
+      }
+
+    </di>
+  )
 }
+function Hora(respuesta=0) {
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+  if (hours > 8 && hours < 14) {
+    timeOfDay = "Buenos dias";
+  } else if (hours >= 14 && hours < 20) {
+    timeOfDay = "Buenas tardes";
+  } else {
+    timeOfDay = "Buenas noches";
+  }
+  if(respuesta==1){
+    return timeOfDay;
+  }else{
+    return <h3>La hora del dia es: {timeOfDay}</h3>
+  }
+}
+function Saludo_Usuario() {
+  const [nombre, setNombre] = useState("");
+
+  function handleChange(event) {
+    setNombre(event.target.value);
+  }
+  return (
+    <div>
+      <h3>Saludando a {nombre}</h3>
+      <input type="text" placeholder="Nombre" onChange={handleChange} />
+      <h3>La hora del dia es: {Hora(1)}</h3>
+    </div>
+  )
+}
+  
 
 export default App;
